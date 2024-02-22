dcd77913d6a8fd12903fa7df0d0970b5322d45fc
artursantana <artursantana843@gmail.com>
2024-02-22 19:47:36 +0000

    trabalhando a resposividade e estilização

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/shop/Shop',
+        permanent: true,
+      },
+    ];
+  },
+
+
     images: {
         domains: ['http2.mlstatic.com'],
       }
 };
 
 export default nextConfig;
+
+
+  
+
 
