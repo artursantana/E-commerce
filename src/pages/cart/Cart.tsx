63bd5e60922ea0be3d6b083c7b9b0e840dab182f
artursantana <artursantana843@gmail.com>
2024-02-24 18:19:13 +0000

    finalizando

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -2,7 +2,7 @@
 
 import CartItems from '../../components/cartitems/CartItems'
 import React from 'react'
-import * as S from './style'
+import * as S from './_style'
 
 const Cart = () => {
   return (
