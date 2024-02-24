6fb1dd9b873b9225c49968f0768c3ea42ae18895
artursantana <artursantana843@gmail.com>
2024-02-24 16:21:37 +0000

    finalizando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import './global.css'
 
 
-export const Page = ({children}: {children: React.ReactNode}) => {
+const Page = ({children}: {children: React.ReactNode}) => {
   return (
     <div>
       {children}
