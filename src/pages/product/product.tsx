631f62f110c7f8b5b7d426e4decffe84d78ac3e9
artursantana <artursantana843@gmail.com>
2024-02-28 17:06:03 +0000

    console product

--- src/pages/product/product.tsx
+++ src/pages/product/product.tsx
@@ -9,12 +9,13 @@ import { useRouter } from 'next/router';
 const Product: React.FC = () => {
   const { data } = useContext(ShopContext)!;
   const router = useRouter();
+  console.log(data)
 
   const { price } = router.query;
   const parsedPrice = typeof price === 'string' ? parseFloat(price) : undefined;
   const product = parsedPrice !== undefined ? data.find((e) => e.price === parsedPrice) : undefined;
 
-  console.log(product == undefined ? `tudo certo ${product}` : `nao deu certo ${product}`)
+  console.log(product ? `tudo certo ${product}` : `nao deu certo ${product}`)
   return (
     
     <S.Container>
