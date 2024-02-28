39cedb50ade75d606e9a58f9e3454d9c9bf33a4d
artursantana <artursantana843@gmail.com>
2024-02-28 15:59:21 +0000

    vercel implemementado

--- src/components/item/Item.tsx
+++ src/components/item/Item.tsx
@@ -17,7 +17,7 @@ const Item: React.FC<ItemProps> = ({ img, price }) => {
     <S.Container>
       <S.ContaineritemPrices>
         {img && (
-          <Link href={{ pathname: '/product/Product', query: { price } }}>
+          <Link href={{ pathname: `/product/Product`, query: { price } }}>
             <Image src={img} width={100} height={100} alt={`Product image - ${price}`}/>
           </Link>
         )}
