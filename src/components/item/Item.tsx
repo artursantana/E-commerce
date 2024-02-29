d2d93bb5133e8fba997c4d3c6e34d34041a1d4a0
artursantana <artursantana843@gmail.com>
2024-02-29 17:32:13 +0000

    links

--- src/components/item/Item.tsx
+++ src/components/item/Item.tsx
@@ -17,7 +17,7 @@ const Item: React.FC<ItemProps> = ({ img, price }) => {
     <S.Container>
       <S.ContaineritemPrices>
         {img && (
-          <Link href={{ pathname:'/product/Product'}}>
+          <Link href='/product/Product'>
             <Image src={img} width={100} height={100} alt={`Product image - ${price}`}/>
           </Link>
         )}
